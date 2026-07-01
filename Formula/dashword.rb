cc96f477d2eb3c44b2213b25d4b321c400d3fdfa
Luke Maciak <maciakl@users.noreply.github.com>
2026-07-01 01:46:50 -0400

    Refactor Dashword formula for architecture-specific URLs
    Updated the Dashword formula to conditionally set the URL and SHA256 based on CPU architecture.

--- Formula/dashword.rb
+++ Formula/dashword.rb
@@ -1,13 +1,18 @@
 class Dashword < Formula
   desc "A memorable password generator"
   homepage "https://github.com/maciakl/dashword"
-  
-  # Link to your online hosted tarball from Step 1
-  url "https://github.com/maciakl/dashword/releases/download/v0.1.4/dashword_Apple_Darwin_aarch64.tar.gz"
-  sha256 "c22784ab8f23368b47b5596be85c6c7f4e85e81e4d8d4bce5681a1275dc6b3a3"
+  version "0.1.4"
 
+   if Hardware::CPU.arm?
+     url "https://github.com/maciakl/dashword/releases/download/v0.1.4/dashword_Apple_Darwin_aarch64.tar.g"
+     sha256 "c22784ab8f23368b47b5596be85c6c7f4e85e81e4d8d4bce5681a1275dc6b3a3"
+     
+  elsif Hardware::CPU.intel?
+     url "https://github.com/maciakl/dashword/releases/download/v0.1.4/dashword_Apple_Dawin_x86_64.tar.gz"
+     sha256 "8411af79457c69e4d3a5c1ef0bca4b26eb46e49c6914789abeeecbea3a631de9"
+  end
+  
   def install
-    # This copies the executable directly into Homebrew's bin directory
     bin.install "dashword"
   end
 end
