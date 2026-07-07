fef9b6b40ee29448c94175dc77cdc31311317b54
Luke Maciak <maciakl@users.noreply.github.com>
2026-07-06 23:01:20 -0400

    Bump mdwi version to 0.4.2 and update URLs

--- Formula/mdwi.rb
+++ Formula/mdwi.rb
@@ -1,17 +1,17 @@
 class Mdwi < Formula
   desc "Static markdown wiki generator"
   homepage "https://github.com/maciakl/mdwi"
-  version "0.4.1"
+  version "0.4.2"
 
   if Hardware::CPU.arm?
     
-    url "https://github.com/maciakl/mdwi/releases/download/v0.4.1/mdwi_0.4.1_Darwin_arm64.tar.gz"
-    sha256 "311ceb879fa0e04787d4c15afbc9272f9b697ae7c63520032306c8516ea48039"
+    url "https://github.com/maciakl/mdwi/releases/download/v0.4.2/mdwi_0.4.2_Darwin_arm64.tar.gz"
+    sha256 "4e0e6f1bdb4e87e2bf1a8b432388b1f5d4da26b3f9870e17f30fe6d059837206"
     
   elsif Hardware::CPU.intel?
     
-    url "https://github.com/maciakl/mdwi/releases/download/v0.4.1/mdwi_0.4.1_Darwin_x86_64.tar.gz"
-    sha256 "276ebbaf673c4773cf5f04b30fe81812d668474a61cef2e0bb0c2e31a6d38526"
+    url "https://github.com/maciakl/mdwi/releases/download/v0.4.2/mdwi_0.4.2_Darwin_x86_64.tar.gz"
+    sha256 "eddb30b80873227434fc571068b64a74bb63e4aebcb4ed30cf1a75c4becab2a2"
     
   end
 
